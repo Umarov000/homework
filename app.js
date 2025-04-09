ccee4359b81a87e911d05ef177d4a9f855f3d0b8
Abdulbosit <umarov@MacBook-Air-Abdulbosit.local>
2025-04-09 17:41:01 +0500

    added loop

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 console.log("Salom GitHub");
 console.log("mysecond commit");
+let arr = [1, 2, 3, 4, 5, 6]
+function func(arr){
+    for(let num of arr){
+        console.log(num);
+    }
+}
+func()
